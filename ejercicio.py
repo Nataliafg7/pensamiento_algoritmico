39b1212aab31665dfb922d3ca3098804f8a9b982
nataliafg7 <nataliaflorezz7282@gmail.com>
2023-03-05 20:17:59 -0500

    ejercicios

---
+++ ejercicio.py
@@ -0,0 +1,150 @@
+# numero= float (input("ingrese un numero ")) 
+# if(numero >0):
+#     print ("el numero es positivo")
+# elif (numero <0):
+#     print ("el numero es negativo")
+# else:
+#     print ("el numero es cero")
+
+
+
+
+# numero1= int (input("ingrese el primer numero "))
+# numero2= int (input("ingrese el segundo numero "))
+# if(numero1>numero2):
+#     print("el primer numero es mayor")
+# else:
+#     print ("el segundo numero es mayor")     
+    
+
+
+
+# x= int(input("ingrese el valor de x "))
+# y= int(input("ingrese el valor de y "))
+
+# if x<= 4/3 and y <5/6 or y >5/6:
+#     c=((4-3*x)**(1/2)) / ((5-6*y)**(1/3))
+#     print("el valor de la expresion es:",c)
+# else:
+#     print("la expresion no posee solucion")
+    
+
+
+
+# s1= float (input("ingrese la primer nota del seguimiento "))
+# s2= float (input("ingrese la segunda nota del seguimiento "))
+# p= float (input("ingrese la nota del parcial "))
+# f= float (input("ingrese la nota del final "))
+# nota=s1*0.2+s2*0.2+p*0.3+f*0.3
+# if(nota>=3.0):
+#     print("el estudiante gano la materia")
+#     print("la nota final es", nota)
+# elif nota>=2.0 and nota<=2.9:
+#     print("el estudiante tiene derecho a habilitar")
+#     print("la nota final es", nota)
+#     habilitacion= float (input("ingrese la nota de la habilitacion "))
+#     if(habilitacion>=3.0):
+#         print("el estudiante gano la materia")
+#     else:
+#         print("el estudiante perdio la materia")
+# elif(nota<2.0):
+#     print ("el estudiante perdio la materia")
+#     print ("la nota final es", nota)
+
+
+
+
+# numero1= int(input("ingrese el primer numero "))
+# numero2= int(input("ingrese el segundo numero "))
+# division=numero1/numero2
+# if numero1 % numero2 ==0:
+#     print("el primer numero es divisible por el segundo")
+#     print("el resultado es", division)
+# else:
+#     print("el primer numero no es divisible por el segundo")
+
+
+
+
+# numero= int(input("ingrese un numero entero de 2 cifras "))
+# if numero % 2==0:
+#     print("el numero es par")
+# if numero % 7==0:
+#     print("el numero es multiplo de 7")
+# if numero % 10==3:
+#     print("su ultima cifra es 3")
+    
+
+
+
+# num1= int(input("ingrese el primer numero entero "))
+# num2= int(input("ingrese el segundo numero entero "))
+# num3= int(input("ingrese el tercer numero entero "))
+# prom=(num1*num2*num3)/3
+# print("el promedio de los numeros es",prom)
+# dis1=abs(prom-num1)
+# dis2=abs(prom-num2)
+# dis3=abs(prom-num3)
+# if dis1<dis2 and dis1<dis3:
+#     print("el numero mas cercano al promedio es", num1)
+# elif dis2<dis1 and dis2<dis3:
+#     print("el numero mas cercano al promedio es", num2)
+# else:
+#    print("el numero mas cercano al promedio es", num3)
+
+
+
+
+# y= input("ingrese el valor de y ")
+# a= input("ingrese el valor de a ")
+# b= input("ingrese el valor de b ")
+# c= input("ingrese el valor de c ")
+
+# if y<a and (a<b) and (b < c):
+#     print("el valor de x es 0")
+    
+# elif (a<=y and y<b):
+#     print("el valor de x es 1")
+    
+# elif (b<=y) and (y<c):
+#     print("el valor de x es 2")
+    
+# elif (c<=y):
+#     print("el valor de x es 3")
+
+# else:
+#     print("el valor de x es 4") 
+    
+
+
+
+# a= int(input("ingrese el peso de la esfera a"))
+# b= int(input("ingrese el peso de la esfera b"))
+# c= int(input("ingrese el peso de la esfera c"))
+# d= int(input("ingrese el peso de la esfera d"))
+
+# if a==b and a==c:
+#     if a>d:
+#         print("la esfera diferente es d y pesa menos")
+#     else:
+#         print ("la esfera diferente es d y pesa mas")
+# elif b==c and b==d:
+#     if b>a:
+#         print("la esfera diferente es a y pesa menos")
+#     else:
+#         print ("la esfera diferente es a y pesa mas")
+# elif c==d and c==a:
+#     if c>b:
+#         print("la esfera diferente es b y pesa menos")
+#     else:
+#         print ("la esfera diferente es b y pesa mas")     
+# elif d==a and d==b:
+#     if d>c:
+#         print("la esfera diferente es c y pesa menos")
+#     else:
+#         print ("la esfera diferente es c y pesa mas")
+
+
+
+
+    
